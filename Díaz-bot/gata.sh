0301d51290e41b560242836414491c10ece4fb45
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-07-02 13:51:11 -0500

    Update gata.sh

--- Díaz-bot/gata.sh
+++ Díaz-bot/gata.sh
@@ -195,11 +195,11 @@ echo -e "\e[35m
 ╚═╝░░╚═╝░░╚═╝░░  ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝\n\e[0m"
 
 echo -e "\033[1;35m"
-git clone https://github.com/FantaBot1/Optimus.git
+git clone https://github.com/diaz1975/Díaz-Bot.git
 echo -e "\033[01;32m\033[01mLa clonación se ha descargado e instalado correctamente.\nThe clone has been downloaded and installed successfully.\n\033[0m"
 
 echo -e "\033[01;32m\033[01mCambiando al directorio del repositorio!!\nChanging to the repository directory!!\n\033[0m" 
-cd GataBot-MD
+cd Díaz-Bot
 
 echo -e "\e[36m
 ██╗░░░██╗██████╗░██████╗░░█████╗░████████╗███████╗  ██╗░░░██╗░█████╗░██████╗░███╗░░██╗
