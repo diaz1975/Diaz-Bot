2a220fe92e706faae4d6c66468eb4c6bc68a0650
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-07-02 02:24:32 -0500

    Update index.js

--- Díaz-bot/index.js
+++ Díaz-bot/index.js
@@ -12,11 +12,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Kchme\nOptimus\nBot', {
+say('KSuper\nDiaz\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Violame JAJAJA`, {
+say(`El mejor`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
