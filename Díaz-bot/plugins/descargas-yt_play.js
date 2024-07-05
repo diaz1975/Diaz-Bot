42afdb4dfb66abfdb89d3aae3d9cd40d32215a6e
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-07-05 03:39:35 -0500

    Update descargas-yt_play.js

--- Díaz-bot/plugins/descargas-yt_play.js
+++ Díaz-bot/plugins/descargas-yt_play.js
@@ -13,7 +13,7 @@ if (command === 'play') {
 additionalText = '𝘼𝙐𝘿𝙄𝙊 🔊'
 } else if (command === 'play2') {
 additionalText = '𝙑𝙄𝘿𝙀𝙊 🎥'}
-let captionvid = `〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕
+let captionvid = `〔 𝗗𝗶𝗮𝘇 𝗕𝗼𝘁🥷🏻 〕
 
 ও 𝙏𝙄𝙏𝙐𝙇𝙊 | 𝙏𝙄𝙏𝙇𝙀
 »  ${yt_play[0].title}
