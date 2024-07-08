6c8bb85df24e58fe6d74626c24217a8aaba4f4d9
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-07-08 18:42:03 -0500

    Update gc-hidetag.js

--- Díaz-bot/plugins/gc-hidetag.js
+++ Díaz-bot/plugins/gc-hidetag.js
@@ -32,7 +32,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: 'https://chat.whatsapp.com/LHo0RmYd3pxAQzp24NYcTV'}}}}}, {});
+      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: 'https://chat.whatsapp.com/BjLnTuDd7Fm4iRL3jUGL5d'}}}}}, {});
     }
   }
 };
