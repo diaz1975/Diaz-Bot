e7c3f4053daf7d922934b44246d9d1fa3c02f48f
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-07-07 21:34:05 -0500

    Add files via upload

--- Díaz-bot/plugins/fun-follar.js
+++ Díaz-bot/plugins/fun-follar.js
@@ -1,17 +1,19 @@
-const handler = async (m, {conn, usedPrefix, command, text}) => {
-  let who;
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
-  else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat;
-  const user = global.db.data.users[who];
-  if (!who) return m.reply(`*✳️ menciona a quien deseas follar*\n\n*📌 ejemplo :*\n${usedPrefix + command} @tag`);
 
+let handler = async (m, { conn, command, text }) => {
+if (!text) return m.reply(`*Ingresa el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`)
+try {
+let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+m.reply(`🤤👅🥵 *𝐀𝐂𝐀𝐁𝐀𝐒 𝐃𝐄 𝐅𝐎𝐋𝐋𝐀𝐑𝐓𝐄𝐋@!*🥵👅🤤
 
-  const abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]}  se esta follando duro a @${who.split('@')[0]} `, m, {mentions: [who, m.sender]});
+*𝙏𝙚 𝙖𝙘𝙖𝙗𝙖𝙨 𝙙𝙚 𝙛𝙤𝙡𝙡𝙖𝙧 𝙖 𝙡𝙖 𝙥𝙚𝙧𝙧𝙖 𝙙𝙚* *${text}* ⁩ *𝙖 𝟰 𝙥𝙖𝙩𝙖𝙨 𝙢𝙞𝙚𝙣𝙩𝙧𝙖𝙨 𝙩𝙚 𝙜𝙚𝙢𝙞𝙖 𝙘𝙤𝙢𝙤 𝙪𝙣𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙥𝙚𝙧𝙧𝙖 "𝐀𝐚𝐚𝐡.., 𝐀𝐚𝐚𝐡𝐡, 𝐬𝐢𝐠𝐮𝐞, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬.." 𝙮 𝙡𝙖 𝙝𝙖𝙨 𝙙𝙚𝙟𝙖𝙙𝙤 𝙩𝙖𝙣 𝙧𝙚𝙫𝙚𝙣𝙩𝙖𝙙𝙖 𝙦𝙪𝙚 𝙣𝙤 𝙥𝙪𝙚𝙙𝙚 𝙨𝙤𝙨𝙩𝙚𝙣𝙚𝙧 𝙣𝙞 𝙨𝙪 𝙥𝙧𝙤𝙥𝙞𝙤 𝙘𝙪𝙚𝙧𝙥𝙤 𝙡𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙯𝙤𝙧𝙧𝙖!*
 
-  conn.sendMessage(m.chat, {react: {text: '👌🏼', key: abrazo.key}});
-};
-handler.help = ['follar @user'];
-handler.tags = ['fun'];
-handler.command = ['follar'];
-handler.group = true;
-export default handler;
+*${text}* 
+🤤🥵 *¡𝐘𝐀 𝐓𝐄 𝐇𝐀𝐍 𝐅𝐎𝐋𝐋𝐀𝐃𝐎!* 🥵🤤`, null, { mentions: [user] })
+} catch (err) {
+}}
+handler.help = ['follar']
+handler.tags = ['fun']
+handler.command = /^(Follar|violar)/i
+
+handler.register = true
+export default handler
