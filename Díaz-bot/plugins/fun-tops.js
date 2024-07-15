ee12b95837171e9726185ed8970fa6ef13056fac
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-07-15 03:20:57 -0500

    Update fun-tops.js

--- Díaz-bot/plugins/fun-tops.js
+++ Díaz-bot/plugins/fun-tops.js
@@ -51,8 +51,144 @@ function handler(m, {groupMetadata, command, conn, participants}) {
     conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
     // conn.sendFile(m.chat, vn, 'otaku.mp3', null, m, true, { type: 'audioMessage', ptt: true })
   }
+  
+  if (command == 'topintegrantes' || command == 'topintegrante') {
+let top = `*_💎TOP 10 L@S MEJORES INTEGRANTES👑_*
+    
+*_1.- 💎 ${user(a)}_* 💎
+*_2.- 👑 ${user(b)}_* 👑
+*_3.- 💎 ${user(c)}_* 💎
+*_4.- 👑 ${user(d)}_* 👑
+*_5.- 💎 ${user(e)}_* 💎
+*_6.- 👑 ${user(f)}_* 👑
+*_7.- 💎 ${user(g)}_* 💎
+*_8.- 👑 ${user(h)}_* 👑
+*_9.- 💎 ${user(i)}_* 💎
+*_10.- 👑 ${user(j)}_* 👑`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+   
+// ------------------------------------------------------------------------------------------------------------------------------------------------   
+   
+if (command == 'toplagrasa' || command == 'topgrasa') {
+let top = `*_Uwu TOP 10 LA GRASA Uwu_* 
+    
+*_1.- Bv ${user(a)} Bv_*
+*_2.- :v ${user(b)} :v_*
+*_3.- :D ${user(c)} :D_*
+*_4.- Owo ${user(d)} Owo_*
+*_5.- U.u ${user(e)} U.u_*
+*_6.- >:v ${user(f)} >:v_*
+*_7.- :'v ${user(g)} :'v_*
+*_8.- ._. ${user(h)} ._._*
+*_9.- :V ${user(i)} :V_*
+*_10.- XD ${user(j)} XD_*`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+   
+// ------------------------------------------------------------------------------------------------------------------------------------------------   
+   
+if (command == 'toppanafrescos' || command == 'toppanafresco') {
+let top = `*_👊TOP 10 PANAFRESCOS👊_* 
+    
+*_1.- 🤑 ${user(a)}_* 🤑
+*_2.- 🤙 ${user(b)}_* 🤙
+*_3.- 😎 ${user(c)}_* 😎
+*_4.- 👌 ${user(d)}_* 👌
+*_5.- 🧐 ${user(e)}_* 🧐
+*_6.- 😃 ${user(f)}_* 😃
+*_7.- 😋 ${user(g)}_* 😋
+*_8.- 🤜 ${user(h)}_* 🤜
+*_9.- 💪 ${user(i)}_* 💪
+*_10.- 😉 ${user(j)}_* 😉`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+   
+// ------------------------------------------------------------------------------------------------------------------------------------------------   
+   
+if (command == 'topshiposters' || command == 'topshipost') {
+let top = `*_😱TOP 10 SHIPOSTERS DEL GRUPO😱_* 
+    
+*_1.- 😈 ${user(a)}_* 😈
+*_2.- 🤙 ${user(b)}_* 🤙
+*_3.- 🥶 ${user(c)}_* 🥶
+*_4.- 🤑 ${user(d)}_* 🤑
+*_5.- 🥵 ${user(e)}_* 🥵
+*_6.- 🤝 ${user(f)}_* 🤝
+*_7.- 😟 ${user(g)}_* 😟
+*_8.- 😨 ${user(h)}_* 😨
+*_9.- 😇 ${user(i)}_* 😇
+*_10.- 🤠 ${user(j)}_* 🤠`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+   
+// ------------------------------------------------------------------------------------------------------------------------------------------------   
+   
+if (command == 'toppajer@s') {
+let top = `*_😏TOP L@S MAS PAJEROS/AS DEL GRUPO💦_* 
+    
+*_1.- 🥵 ${user(a)}_* 💦
+*_2.- 🥵 ${user(b)}_* 💦
+*_3.- 🥵 ${user(c)}_* 💦
+*_4.- 🥵 ${user(d)}_* 💦
+*_5.- 🥵 ${user(e)}_* 💦
+*_6.- 🥵 ${user(f)}_* 💦
+*_7.- 🥵 ${user(g)}_* 💦
+*_8.- 🥵 ${user(h)}_* 💦
+*_9.- 🥵 ${user(i)}_* 💦
+*_10.- 🥵 ${user(j)}_* 💦`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+   
+// ------------------------------------------------------------------------------------------------------------------------------------------------   
+   
+if (command == 'toplind@s' || command == 'toplindos') {
+let top = `*_😳TOP L@S MAS LIND@S Y SEXIS DEL GRUPO😳_*
+    
+*_1.- ✨ ${user(a)}_* ✨
+*_2.- ✨ ${user(b)}_* ✨
+*_3.- ✨ ${user(c)}_* ✨
+*_4.- ✨ ${user(d)}_* ✨
+*_5.- ✨ ${user(e)}_* ✨
+*_6.- ✨ ${user(f)}_* ✨
+*_7.- ✨ ${user(g)}_* ✨
+*_8.- ✨ ${user(h)}_* ✨
+*_9.- ✨ ${user(i)}_* ✨
+*_10.- ✨ ${user(j)}_* ✨`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+   
+// ------------------------------------------------------------------------------------------------------------------------------------------------   
+   
+if (command == 'topput@s') {
+let top = `*_😏TOP L@S MAS PUT@S DEL GRUPO SON🔥_* 
+    
+*_1.- 👉 ${user(a)}_* 👌
+*_2.- 👉 ${user(b)}_* 👌
+*_3.- 👉 ${user(c)}_* 👌
+*_4.- 👉 ${user(d)}_* 👌
+*_5.- 👉 ${user(e)}_* 👌
+*_6.- 👉 ${user(f)}_* 👌
+*_7.- 👉 ${user(g)}_* 👌
+*_8.- 👉 ${user(h)}_* 👌
+*_9.- 👉 ${user(i)}_* 👌
+*_10.- 👉 ${user(j)}_* 👌`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+   
+// ------------------------------------------------------------------------------------------------------------------------------------------------   
+   
+if (command == 'topfamosos' || command == 'topfamos@s') {
+let top = `*_🌟TOP PERSONAS FAMOSAS EN EL GRUPO🌟_* 
+    
+*_1.- 🛫 ${user(a)}_* 🛫
+*_2.- 🥂 ${user(b)}_* 🥂
+*_3.- 🤩 ${user(c)}_* 🤩
+*_4.- 🛫 ${user(d)}_* 🛫
+*_5.- 🥂 ${user(e)}_* 🥂
+*_6.- 🤩 ${user(f)}_* 🤩
+*_7.- 🛫 ${user(g)}_* 🛫
+*_8.- 🥂 ${user(h)}_* 🥂
+*_9.- 🤩 ${user(i)}_* 🤩
+*_10.- 🛫 ${user(j)}_* 🛫`
+m.reply(top, null, { mentions: conn.parseMention(top) })}
+  
 }
-handler.help = handler.command = ['topgays', 'topotakus'];
+
+handler.help = handler.command = ['topgays', 'topotakus', 'topintegrantes', 'toplagrasa', 'toppanafrescos', 'topshiposters', 'toppajer@s', 'toplind@s', 'topput@s', 'topfamosos'];
 handler.tags = ['games'];
 handler.group = true;
 export default handler;
